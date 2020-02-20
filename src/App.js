aa4373a0c014cb87f03197aacd905767b1e92de9
shawn <shawnpatel196@gmail.com>
2020-02-20 14:15:35 -0600

    finished mvp

--- src/App.js
+++ src/App.js
@@ -7,35 +7,38 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+//contexts
+import {ProductContext} from "./contexts/ProductContext"
+import {CartContext} from "./contexts/CartContext"
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
+		setCart([...cart, item])
 		// add the given item to the cart
 	};
+	// const removeItem = item=>{
+	// 	setCart({...cart,item})      // need filter method here
+	// }
 
 	return (
+		<ProductContext.Provider value={{products, addItem}}>
+			<CartContext.Provider value={{cart}}>
 		<div className="App">
 			<Navigation cart={cart} />
 
 			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+			<Route exact path="/" component={Products} />
 
 			<Route
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
 			/>
 		</div>
+		</CartContext.Provider>
+		</ProductContext.Provider>
 	);
 }
 
